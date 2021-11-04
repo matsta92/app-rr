32a048ce0be85b63bec021553775ff235cb48ba2
Mateusz Stawiarz <matsta92@gmail.com>
2021-11-04 17:52:20 +0100

    Updates

---
+++ precache-manifest.29fcf55937fbec3d2e8d3fc3f2ed3463.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "84723491daee1398a0c8",
+    "url": "/app-rr/static/css/main.06b0d8a2.chunk.css"
+  },
+  {
+    "revision": "84723491daee1398a0c8",
+    "url": "/app-rr/static/js/main.f3211bb9.chunk.js"
+  },
+  {
+    "revision": "9347ed20ee693a8d157a",
+    "url": "/app-rr/static/js/runtime~main.fe775957.js"
+  },
+  {
+    "revision": "4b85e06c6d8c68f22406",
+    "url": "/app-rr/static/js/2.8cb963a3.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-rr/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-rr/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-rr/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "6889dd6c92ee1cfd6973b7eb7a59113a",
+    "url": "/app-rr/index.html"
+  }
+];
